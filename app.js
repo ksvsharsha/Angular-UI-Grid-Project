30a99b20226fe32d5bed993f1f4f732b27989ba9
ksvsharsha <ksvsharsha2010@gmail.com>
2016-02-11 17:07:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['ngTouch', 'ui.grid','ui.grid','ui.grid.edit', 'ui.grid.cellNav', 'ui.grid.exporter', 'ui.grid.selection']);
+var app = angular.module('app', ['ngTouch', 'ngAnimate' ,'ui.grid','ui.grid.edit','ui.grid.resizeColumns', 'ui.grid.pagination','ui.grid.moveColumns', 'ui.grid.cellNav', 'ui.grid.exporter', 'ui.grid.selection']);
 
 app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($scope, $http, $interval, $q) {
   var fakeI18n = function( title ){
@@ -16,38 +16,41 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($sco
     enableGridMenu: true,
     gridMenuTitleFilter: fakeI18n,
    columnDefs: [
-      { name: 'SNo' ,enableCellEdit: false,width: '5%'},
-      {name: 'ActivityDate',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
-      { name: 'EmployeeName' ,enableCellEdit:true ,width: '20%'},
-      { name: 'Program',editableCellTemplate: 'ui-grid/dropdownEditor', width: '15%',
+      { name: 'SNo' ,  enableCellEdit: false,width: '5%'},
+      {name: 'ActivityDate',cellTooltip: 'Double Click To Edit',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
+      { name: 'EmployeeName' ,enableCellEdit:true ,width: '20%',cellTooltip: 'Double Click To Edit'},
+      { name: 'Program',cellTooltip: 'Double Click To Edit',editableCellTemplate: 'ui-grid/dropdownEditor', width: '15%',
       cellFilter: 'mapProgram', editDropdownValueLabel: 'Program', editDropdownOptionsArray: [
       { id: 1, Program: 'IB Services - Spartan' },
       { id: 2, Program: 'CCD - MultiSpeciality' },
       { id: 3, Program:'IB Services - Tejas'  },
       {id: 4, Program: 'IB Services - WIIFIT'}]},
-      { name: 'SprintNumber',enableCellEdit:true,width: '10%' },
-      {name:'SprintStartDate',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
-      {name:'SprintEndDate',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
+      { name: 'SprintNumber',enableCellEdit:true,width: '10%' ,cellTooltip: 'Double Click To Edit'},
+      {name:'SprintStartDate',cellTooltip: 'Double Click To Edit',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
+      {name:'SprintEndDate',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%',cellTooltip: 'Double Click To Edit'},
       {name:'Activity',editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',
-      cellFilter: 'mapActivity', editDropdownValueLabel: 'Activity', editDropdownOptionsArray: [
+      cellFilter: 'mapActivity', editDropdownValueLabel: 'Activity',cellTooltip: 'Double Click To Edit', editDropdownOptionsArray: [
       { id: 1, Activity: 'Coding' },
       { id: 2, Activity: 'Testing' },
       { id: 3, Activity:'No Activity Assigned'  }
       ]},
-      {name:'Category',editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',
+      {name:'Category',editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',cellTooltip: 'Double Click To Edit',
       cellFilter: 'mapCategory', editDropdownValueLabel: 'Category', editDropdownOptionsArray: [
       { id: 1, Category: 'User Story' },
       { id: 2, Category: 'SPR' },
       { id: 3, Category:'ALM Defect'  }
       ]},
-      {name:'ReferenceID',enableCellEdit:true,width: '10%'},
-      {name:'Status',editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',
+      {name:'ReferenceID', enableCellEdit:true,width: '10%',cellTooltip: 'Double Click To Edit'},
+      {name:'Status',cellClass : function(grid, row, col, rowRenderIndex, colRenderIndex) {
+        return "";
+      }, editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',cellTooltip: 'Double Click To Edit',
       cellFilter: 'mapStatus', editDropdownValueLabel: 'Status', editDropdownOptionsArray: [
       { id: 1, Status: 'On Track' },
       { id: 2, Status: 'Possibility of Risk' },
       { id: 3, Status:'At Risk'  },
-      ]},
-      {name:'Comments',enableCellEdit:true, width:'20%'}
+      ]
+    },
+      {name:'Comments',enableCellEdit:true, width:'20%',cellTooltip: 'Double Click To Edit'}
       
     ],
     gridMenuCustomItems: [
@@ -82,12 +85,40 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($sco
 $scope.filter = function() {
     $scope.gridApi.grid.refresh();
   };
-    
+
+  
+  
+  /*Function to Add New Rows*/
+    $scope.addData = function() {
+    var n = $scope.gridOptions.data.length + 1;
+    $scope.gridOptions.data.push({
+                "SNo": n,
+                "ActivityDate":"",
+                "EmployeeName":"",
+                "Program": "",
+                "SprintNumber": "",
+                "SprintStartDate":"",
+                "SprintEndDate":"",
+                "Activity":"",
+                "Category":"",
+                "ReferenceID":"",
+                "Status":"",
+                "Comments":""
+              });
+  };
+
+  /*Function to Delete selected row*/
+  $scope.deleteSelected = function(){
+      angular.forEach($scope.gridApi.selection.getSelectedRows(), function (data, index) {
+        $scope.gridOptions.data.splice($scope.gridOptions.data.lastIndexOf(data), 1);
+      });
+    }
+
   $scope.singleFilter = function( renderableRows ){
     var matcher = new RegExp($scope.filterValue);
     renderableRows.forEach( function( row ) {
       var match = false;
-      [ 'SNo', 'ActivityDate', 'EmployeeName','Program','SprintNumber','SprintStartDate','SprintEndDate','Activity','Category','ReferenceID','Status','Comments' ].forEach(function( field ){
+      [  'ActivityDate', 'EmployeeName','Program','SprintNumber','SprintStartDate','SprintEndDate','Activity','Category','ReferenceID','Status','Comments' ].forEach(function( field ){
         if ( row.entity[field].match(matcher) ){
           match = true;
         }
