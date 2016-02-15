3c681a66f5535e0464c41a7d78295eeed3ec89e4
ksvsharsha <ksvsharsha2010@gmail.com>
2016-02-15 19:16:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,16 +8,23 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($sco
     }, 1000, 1);
     return deferred.promise;
   };
+  var database = TAFFY().store("DAT");
 
+var getdata = database().get("DAT");
+//database({SNo:"1"}).update({EmployeeName:"asdfasdf sdg asdf asdf"});
+//getdata = database().get("DAT");
+  $scope.msg = {};
+  debugger;
   $scope.gridOptions = {
-        enableFiltering: false,
+        enableFiltering: true,
 
-    exporterMenuCsv: false,
+    exporterMenuCsv: true,
     enableGridMenu: true,
-    gridMenuTitleFilter: fakeI18n,
+    
    columnDefs: [
-      { name: 'SNo' ,  enableCellEdit: false,width: '5%'},
-      {name: 'ActivityDate',cellTooltip: 'Double Click To Edit',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
+      { name:'SNo', enableFiltering:false, enableCellEdit: false,width: '7%'
+      },
+      {name: 'ActivityDate',cellTooltip: 'Double Click To Edit',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '15%'},
       { name: 'EmployeeName' ,enableCellEdit:true ,width: '20%',cellTooltip: 'Double Click To Edit'},
       { name: 'Program',cellTooltip: 'Double Click To Edit',editableCellTemplate: 'ui-grid/dropdownEditor', width: '15%',
       cellFilter: 'mapProgram', editDropdownValueLabel: 'Program', editDropdownOptionsArray: [
@@ -25,9 +32,9 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($sco
       { id: 2, Program: 'CCD - MultiSpeciality' },
       { id: 3, Program:'IB Services - Tejas'  },
       {id: 4, Program: 'IB Services - WIIFIT'}]},
-      { name: 'SprintNumber',enableCellEdit:true,width: '10%' ,cellTooltip: 'Double Click To Edit'},
-      {name:'SprintStartDate',cellTooltip: 'Double Click To Edit',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%'},
-      {name:'SprintEndDate',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '10%',cellTooltip: 'Double Click To Edit'},
+      { name: 'SprintNumber',enableCellEdit:true,width: '15%' ,cellTooltip: 'Double Click To Edit'},
+      {name:'SprintStartDate',cellTooltip: 'Double Click To Edit',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '15%'},
+      {name:'SprintEndDate',type:'date', cellFilter:'date:"yyyy-MM-dd"',width: '15%',cellTooltip: 'Double Click To Edit'},
       {name:'Activity',editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',
       cellFilter: 'mapActivity', editDropdownValueLabel: 'Activity',cellTooltip: 'Double Click To Edit', editDropdownOptionsArray: [
       { id: 1, Activity: 'Coding' },
@@ -40,15 +47,25 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($sco
       { id: 2, Category: 'SPR' },
       { id: 3, Category:'ALM Defect'  }
       ]},
-      {name:'ReferenceID', enableCellEdit:true,width: '10%',cellTooltip: 'Double Click To Edit'},
-      {name:'Status',cellClass : function(grid, row, col, rowRenderIndex, colRenderIndex) {
-        return "";
-      }, editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',cellTooltip: 'Double Click To Edit',
+      {name:'ReferenceID', enableCellEdit:true,width: '15%',cellTooltip: 'Double Click To Edit'},
+      {name:'Status',
+       editableCellTemplate: 'ui-grid/dropdownEditor', width: '10%',cellTooltip: 'Double Click To Edit',
+       cellClass: function(grid,row,col,rowRenderIndex,colRenderIndex){
+        if(row.entity.Status == 1)
+          return "green";
+        else if(row.entity.Status == 2){
+          return "amber";
+        }
+        else
+          return "red";
+       },
+        
       cellFilter: 'mapStatus', editDropdownValueLabel: 'Status', editDropdownOptionsArray: [
       { id: 1, Status: 'On Track' },
       { id: 2, Status: 'Possibility of Risk' },
       { id: 3, Status:'At Risk'  },
       ]
+
     },
       {name:'Comments',enableCellEdit:true, width:'20%',cellTooltip: 'Double Click To Edit'}
       
@@ -62,18 +79,31 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', function ($sco
         order: 210
       }
     ],
-     data : [{SNo:"1",ActivityDate:"1stFeb,16",EmployeeName:"Shaik Abdul Rawoof",
-      Program:"1",SprintNumber:"Giza Sprint 5",SprintStartDate:"20-Jan-16",SprintEndDate:"2-Feb-16",
-      Activity:"1",Category:"1",ReferenceID:"64778",Status:"1",Comments:"All Good"
-      },
-      {SNo:"2",ActivityDate:"2ndFeb16",EmployeeName:"Harsha",Program:"2",SprintNumber:"Giza Sprint 6",SprintStartDate:"10-feb-2016",
-        SprintEndDate:"5-Feb-16",Activity:"2",Category:"2",ReferenceID:"438282",Status:"2",Comments:"Good"}],
+     data : getdata,
     onRegisterApi: function( gridApi ){
       $scope.gridApi = gridApi;
-      $scope.gridApi.grid.registerRowsProcessor( $scope.singleFilter, 200 );
+      gridApi.edit.on.afterCellEdit($scope,function(rowEntity, colDef, newValue, oldValue){
+        $scope.gridOptions.cellClass = $scope.getStatus(rowEntity);
+        var json = {};
+         json[colDef.name] = newValue;
+           database({SNo:rowEntity.SNo}).update(json);
+localStorage.setItem( 'taffy_DAT',
+                  JSON.stringify( database().get()) );
 
+         });
+
+
+$scope.getStatus = function(rowEntity){
+        if(rowEntity.Status == 1)
+          return "green";
+        else if(rowEntity.Status == 2){
+          return "amber";
+        }
+        else
+          return "red";
+       }
       // interval of zero just to allow the directive to have initialized
-      $interval( function() {
+     $interval( function() {
         gridApi.core.addToGridMenu( gridApi.grid, [{ title: 'Dynamic item', order: 100}]);
       }, 0, 1);
 
@@ -91,7 +121,7 @@ $scope.filter = function() {
   /*Function to Add New Rows*/
     $scope.addData = function() {
     var n = $scope.gridOptions.data.length + 1;
-    $scope.gridOptions.data.push({
+    var row  = {
                 "SNo": n,
                 "ActivityDate":"",
                 "EmployeeName":"",
@@ -104,15 +134,29 @@ $scope.filter = function() {
                 "ReferenceID":"",
                 "Status":"",
                 "Comments":""
-              });
+              }
+    $scope.gridOptions.data.push(row);
+
+
+   database.insert(row);
+
+            
+
+
   };
 
   /*Function to Delete selected row*/
-  $scope.deleteSelected = function(){
+  $scope.deleteSelected = function(rowEntity){
+
+
       angular.forEach($scope.gridApi.selection.getSelectedRows(), function (data, index) {
-        $scope.gridOptions.data.splice($scope.gridOptions.data.lastIndexOf(data), 1);
+      $scope.gridOptions.data.splice($scope.gridOptions.data.lastIndexOf(data), 1);
+      database({SNo:data.SNo}).remove();
       });
+
     }
+
+    
 
   $scope.singleFilter = function( renderableRows ){
     var matcher = new RegExp($scope.filterValue);
